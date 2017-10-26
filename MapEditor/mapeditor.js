e1c2bfa134f62ba6ea4a34aa5d01eaf74b2c6087
ZC <zengcheng263@gmail.com>
2017-10-27 01:49:15 +0800

    map Editor step one

---
+++ MapEditor/mapeditor.js
@@ -0,0 +1,7 @@
+/*首先创建一个png*/
+
+var img = document.createElement("img");
+img.src = "normal_brick4.png";
+img.style.position = "absolute";
+img.style.top = "100px";
+document.body.appendChild(img);

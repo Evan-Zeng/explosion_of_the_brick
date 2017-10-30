4a61d1ed9a18663dfd5a1c02928ce06b7a7f2a95
Evan-Zeng <745995421@qq.com>
2017-10-31 01:36:05 +0800

    Signed-off-by: Evan-Zeng <745995421@qq.com>
    map editor update

--- MapEditor/mapeditor.js
+++ MapEditor/mapeditor.js
@@ -1,7 +1,52 @@
 /*首先创建一个png*/
 
-var img = document.createElement("img");
-img.src = "normal_brick4.png";
-img.style.position = "absolute";
-img.style.top = "100px";
-document.body.appendChild(img);
+var gamemap = document.getElementById('div_game_map');
+var subDivWidth = 1320;
+var subDivHeight = 20;
+var subDivNum = 20;
+
+var bricksWidth = 60;
+
+/*
+	创建一个全局的div
+
+	里面创建20个1320x20的子div
+
+	每个子div有20个img
+
+*/
+
+/*
+	这里不用关心一个子div的高度，反正我们只是生成地图供游戏读取
+*/
+function CreateChildrenDiv()
+{
+	var position = { x: 0, y: 0 };
+	var bricks_per_row = subDivWidth / bricksWidth;
+	for (var idx = 0; idx < subDivNum; idx++)
+	{
+		position.y = idx * subDivHeight;
+		var subDiv = document.createElement("div");
+		subDiv.id = "subDiv_" + idx.toString();
+		subDiv.style.display = "block";
+		subDiv.style.top = position.y.toString() + "px";
+		subDiv.style.width = subDivWidth.toString() + "px";
+		subDiv.style.height = subDivHeight.toString() + "px";
+
+		for (var imgIdx = 0; imgIdx < bricks_per_row; imgIdx++)
+		{
+			var img = document.createElement("img");
+			img.src = "default_brick.png";
+			subDiv.appendChild(img);
+		}
+
+		gamemap.appendChild(subDiv);
+	}
+}
+
+function Init()
+{
+	var selectElement = document.getElementById("select_brick");
+}
+
+CreateChildrenDiv();
